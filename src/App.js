3a6c9bc9af0f4e0ad24411ff33d79d7a31417999
Dino Hadzic <zgdino@yahoo.com>
2022-04-07 21:14:01 +0200

    hello

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css'
 
-function App() {
-  return <div className='App'></div>
+const App =() => {
+  return <div className='App'>
+    <h1>Hello my dear Friend</h1>
+  </div>
 }
 
 export default App
