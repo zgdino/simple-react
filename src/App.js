da7a4803269f726bf467711a91855f94b5cccdf5
Dino Hadzic <zgdino@yahoo.com>
2022-04-07 21:09:03 +0200

    small

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>{2+2}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
