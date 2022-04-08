d9e23135d4ccf10f084ee1dae0949d47bc5f86ab
Dino Hadzic <zgdino@yahoo.com>
2022-04-08 23:24:25 +0200

    little change

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 const App =() => {
   const name = 'Lulu'
   const isNameShowing = false
+  // jsx
   return <div className='App'>
     <h1>Hello {isNameShowing ? name : 'someone'}</h1>
   </div>
