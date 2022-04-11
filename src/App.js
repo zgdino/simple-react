4cb57bcf15063a509e769f8454cd855f24f6b69d
Dino Hadzic <zgdino@yahoo.com>
2022-04-11 22:32:12 +0200

    Person component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css'
 
-const App =() => {
+const Person = () => {
+  return (
+    <>
+      <h1>Name: John</h1>
+      <h2>Last Name: Doe</h2>
+      <h2>Age: 30</h2>
+    </>
+  )
+}
+
+const App = () => {
   // jsx
-  return <div className='App'>
-    <h1>Hello {2+2}</h1>
-  </div>
+  return (
+    <div className='App'>
+      <h1>Hello {2 + 2}</h1>
+    </div>
+  )
 }
 
 export default App
