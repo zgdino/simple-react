d8afe8046eab8d1fd1f0cee1cf9bdaccf8da5541
Dino Hadzic <zgdino@yahoo.com>
2022-04-12 11:05:26 +0200

    playing with props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const Person = (props) => {
   return (
     <>
       <h1>Name: {props.name}</h1>
-      <h2>Last Name: Doe</h2>
+      <h2>Last Name: {props.lastName}</h2>
       <h2>Age: 30</h2>
     </>
   )
@@ -15,7 +15,7 @@ const App = () => {
   return (
     <div className='App'>
       <Person name={'John'} />
-      <Person />
+      <Person lastName={'Doe'} />
       <Person />
       <Person />
     </div>
