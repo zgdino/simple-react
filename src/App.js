28a332dafafed6b2ef34fb6d9a28050902857e96
Dino Hadzic <zgdino@yahoo.com>
2022-04-14 22:50:17 -1000

    testing the bolier plate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import './App.css'
-import { useState, useEffect } from 'react'
+import React from "react";
 
 const App = () => {
-  // useState HOOK
-  const [counter, setCounter] = useState(0)
-  // useEffect HOOK → never modify the state manually; 
-  // if dependency array is empty → change only on the initial load
-  useEffect(() => {
-    alert('you have changed the counter to' + counter)
-  }, [counter]);
-
-  return (
-    <div className='App'>
-      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
-        -
-      </button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
-        +
-      </button>
-    </div>
-  )
+ return (
+  <h1>App</h1>
+ )
 }
 
 export default App
