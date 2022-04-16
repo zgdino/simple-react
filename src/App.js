4bab5b6653477c48e37983a5dfa1c31ac3b813ce
Dino Hadzic <zgdino@yahoo.com>
2022-04-16 11:51:39 +0200

    test movie appearance

--- src/App.js
+++ src/App.js
@@ -35,10 +35,17 @@ const App = () => {
           value='Superman'
           onChange={() => {}}
         />
-        <img src={SearchIcon} alt="search" onClick={() => {}} />
+        <img src={SearchIcon} alt='search' onClick={() => {}} />
       </div>
-      <div className="container">
-
+      <div className='container'>
+        <div className='movie'>
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+          <div>
+           <img src={movie1.Poster} alt={movie1.Title} />
+          </div>
+        </div>
       </div>
     </div>
   )
