095d130ca7a00aeb4137bcaa1738859ec94e19e5
Dino Hadzic <zgdino@yahoo.com>
2022-04-15 22:48:22 -1000

    movie app start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React from 'react'
+import { useEffect } from 'react'
+
+const API_URL = 'http://www.omdbapi.com?apikey=6333b19f'
 
 const App = () => {
- return (
-  <h1>App</h1>
- )
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json()
+
+    console.log(data.Search)
+  }
+
+  useEffect(() => {
+   searchMovies('spiderman')
+  }, [])
+
+  return <h1>App</h1>
 }
 
 export default App
