1b702486032bf38b541cfb33ca8c956a1b47e05f
Dino Hadzic <zgdino@yahoo.com>
2022-04-18 12:16:35 +0200

    search functionality

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ const movie1 = {
 
 const App = () => {
   const [movies, setMovies] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`)
@@ -35,10 +36,10 @@ const App = () => {
       <div className='search'>
         <input
           placeholder='Search for movies'
-          value='Superman'
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <img src={SearchIcon} alt='search' onClick={() => {}} />
+        <img src={SearchIcon} alt='search' onClick={() => searchMovies(searchTerm)} />
       </div>
 
       {movies?.length > 0 ? (
